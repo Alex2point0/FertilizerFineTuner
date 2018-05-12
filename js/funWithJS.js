dff49329c4a9a6038e235c26a41b09bc5fffd436
zaxbatch <34843086+zaxbatch@users.noreply.github.com>
2018-05-11 22:12:49 -0400

    added PeKacid and Potassium Nitrate

--- js/funWithJS.js
+++ js/funWithJS.js
@@ -1,11 +1,11 @@
 document.getElementById('wrapper01').innerHTML = "<hr>Adjust grams";
-document.getElementById('wrapper02').innerHTML = "<br>Calcium Nitrate<br><input id='adjustCalNit' type='number'><br><br>MPK<br><input id='adjustMPK' type='number'><br><br>Epsom Salt<br><input id='adjustEpsom' type='number'><br><br>Trace Elements<br><input id='adjustTrace' type='number'><br><br>";
+document.getElementById('wrapper02').innerHTML = "<br>Calcium Nitrate<br><input id='adjustCalNit' type='number'><br><br>Potassium Nitrate<br><input id='adjustPotNit' type='number'><br><br>MPK<br><input id='adjustMPK' type='number'><br><br>PeKacid<br><input id='adjustPekacid' type='number'><br><br>Epsom Salt<br><input id='adjustEpsom' type='number'><br><br>Trace Elements<br><input id='adjustTrace' type='number'><br><br>";
 
 function ppmCalc() {
 var ppmN, ppmP, ppmK, ppmCa, ppmMg,ppmFe;
-ppmN = document.getElementById('adjustCalNit').value*41;
-ppmP = document.getElementById('adjustMPK').value*60;
-ppmK = document.getElementById('adjustMPK').value*75;
+ppmN = (document.getElementById('adjustCalNit').value*41) + (document.getElementById('adjustPotNit').value*36);
+ppmP = (document.getElementById('adjustMPK').value*60) + (document.getElementById('adjustPekacid').value*69);
+ppmK = (document.getElementById('adjustMPK').value*75) + (document.getElementById('adjustPotNit').value*101) + (document.getElementById('adjustPekacid').value*44);
 ppmCa = document.getElementById('adjustCalNit').value*50;
 ppmMg = document.getElementById('adjustEpsom').value*25;
 ppmFe = document.getElementById('adjustTrace').value*19.8;
